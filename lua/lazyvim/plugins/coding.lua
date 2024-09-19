13bccc9d0bdf897b57c30adecaa41c30231516e4
Aloe <magnetischen@gmail.com>
2024-09-19 01:18:10 -0700

    Remove tab and super+tab keybindings

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -103,27 +103,7 @@ return {
       if LazyVim.has("nvim-snippets") then
         table.insert(opts.sources, { name = "snippets" })
       end
-    end,
-    keys = {
-      {
-        "<Tab>",
-        function()
-          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-      {
-        "<S-Tab>",
-        function()
-          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-    },
+    end
   },
 
   -- auto pairs
